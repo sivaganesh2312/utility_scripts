9eafa66a61c80f53d2d34c72418d83186f203bd8
ganesh <ganesh@email.com>
2024-04-23 14:30:32 +0530

    added pytest

---
+++ python/temp.py
@@ -0,0 +1,14 @@
+def print_numbers(start, end):
+    """print Numbers
+
+    Args:
+      start: start value
+      end: end value
+
+    Returns: None
+
+    """
+    for index in range(start,end):
+        print(index)
+
+print_numbers(1,10)        
