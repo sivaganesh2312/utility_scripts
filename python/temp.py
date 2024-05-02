edbba371bd60f92dde9e18687b0ce766614b8b8c
ganesh <ganesh@email.com>
2024-05-02 10:00:18 +0530

    added tuples

--- python/temp.py
+++ python/temp.py
@@ -12,3 +12,15 @@ def print_numbers(start, end):
         print(index)
 
 print_numbers(1,10)        
+
+
+
+# code to print all the even numbers from start to end
+def is_even(number):
+  if number % 2 == 0:
+    return true
+
+def even_numbers(start, end):
+  numbers = list(range(start, end))
+  return ...
+    
